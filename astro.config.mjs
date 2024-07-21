a0a1ff5ca632937a26f3ec266b49529ace070d0a
Brady Blair <brady@refactoredmedia.com>
2024-07-20 23:07:06 -0400

    chore: Update npm dependencies and tech stack information

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,57 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
-
-// https://astro.build/config
 import sitemap from "@astrojs/sitemap";
+import AstroPWA from '@vite-pwa/astro';
+
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sitemap()],
+  site: 'https://www.bradyblair.com',
+  integrations: [tailwind(), sitemap(), AstroPWA({
+    mode: import.meta.env.PROD ? 'production' : 'development',
+    base: '/',
+    scope: '/',
+    includeAssets: ['favicon.svg'],
+    registerType: 'autoUpdate',
+    manifest: {
+      name: 'Brady Blair Portfolio',
+      short_name: 'Brady Blair Portfolio',
+      theme_color: '#ffffff',
+      icons: [{
+        src: 'android-chrome-192x192.png',
+        sizes: '192x192',
+        type: 'image/png'
+      }, {
+        src: 'android-chrome-512x512.png',
+        sizes: '512x512',
+        type: 'image/png'
+      }, {
+        src: 'android-chrome-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+        purpose: 'any maskable'
+      }]
+    },
+    workbox: {
+      navigateFallback: '/',
+      globPatterns: ['**/*.{css,js,html,svg,png,ico,txt}']
+    },
+    devOptions: {
+      enabled: true,
+      navigateFallbackAllowlist: [/^\//]
+    },
+    experimental: {
+      directoryAndTrailingSlashHandler: true
+    }
+  }),
+  partytown()
+  ],
   output: "static",
-  site: 'https://www.bradyblair.com'
+  prefetch: {
+    'prefetchAll': true
+  },
+  build: {
+    inlineStylesheets: 'always'
+  },
 });
